a74ae6a36038515ef2060977490f2b10bb262280
Marko Helin <marko@koroun.se>
2021-12-12 01:10:07 +0100

    Add aoc2021 11

---
+++ 11/part1.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+
+
+def input(file_name):
+    m = defaultdict(lambda: defaultdict(lambda: -1))
+    with open(file_name) as f:
+        l = 0
+        for line in f:
+            for k in range(0, len(line.strip())):
+                m[l][k] = int(line[k])
+            l += 1
+        return m
+
+
+def oob(r, c, octos):
+    return r < 0 or r >= len(octos) or c < 0 or c >= len(octos[0])
+
+
+def flash(octos, r, c, flashed):
+    if flashed[r][c] == 1:
+        return
+    flashed[r][c] = 1
+
+    adj = [
+        (r - 1, c),
+        (r - 1, c + 1),
+        (r - 1, c - 1),
+        (r, c + 1),
+        (r, c - 1),
+        (r + 1, c),
+        (r + 1, c + 1),
+        (r + 1, c - 1),
+    ]
+
+    for a in adj:
+        ar, ac = a
+        if oob(ar, ac, octos):
+            continue
+        octos[ar][ac] += 1
+        if octos[ar][ac] > 9 and not flashed[ar][ac]:
+            flash(octos, ar, ac, flashed)
+
+
+def evolve(octos, flashed):
+    for r in range(len(octos)):
+        for c in range(len(octos[0])):
+            octos[r][c] += 1
+    for r in range(len(octos)):
+        for c in range(len(octos[0])):
+            if octos[r][c] > 9:
+                flash(octos, r, c, flashed)
+    flash_count = 0
+    for r in range(len(octos)):
+        for c in range(len(octos[0])):
+            if flashed[r][c] == 1:
+                flash_count += 1
+                octos[r][c] = 0
+            flashed[r][c] = 0
+    return flash_count
+
+
+def main():
+    octos = input("input.txt")
+    flashed = defaultdict(lambda: defaultdict(lambda: 0))
+    flash_count = 0
+    for step in range(100):
+        flash_count += evolve(octos, flashed)
+    print(f"after step {step+1}, flashes {flash_count}")
+
+
+if __name__ == "__main__":
+    main()
